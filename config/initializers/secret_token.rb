bb010e5193dc538d563ccbdf49a792133664c3d5
Ryan Pope <ryanpope@post.harvard.edu>
2013-03-23 01:01:40 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SamplePp::Application.config.secret_token = 'c90322132f4f62cb0db9d25cdb3064919de7249fe53c660102f77a9998fdbaab06d302e9ae733df0f343984b9f0bcad31512f88ac81d6d3bf409e464a58ec500'
